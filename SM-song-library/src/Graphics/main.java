94e799e34a588a7db80d78226c295e62a3440906
jayp13997 <jayp13997@gmail.com>
2020-02-15 13:56:02 -0500

    Added some things

--- SM-song-library/src/Graphics/main.java
+++ SM-song-library/src/Graphics/main.java
@@ -1,5 +1,25 @@
 package Graphics;
 
-public class main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+
+public class main extends Application {
+
+	 @Override
+	    public void start(Stage primaryStage) throws Exception{
+	        Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+	        primaryStage.setTitle("Song Library");
+	        primaryStage.setScene(new Scene(root, 400, 300));
+	        primaryStage.show();
+	    }
+
+
+	    public static void main(String[] args) {
+	        launch(args);
+	    }
+	
 }
