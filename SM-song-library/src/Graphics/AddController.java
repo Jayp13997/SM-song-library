96efa29c93ea9609375275e10c8e4aef200656e5
jayp13997 <jayp13997@gmail.com>
2020-02-16 16:12:30 -0500

    More UI stuff

---
+++ SM-song-library/src/Graphics/AddController.java
@@ -0,0 +1,21 @@
+package Graphics;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AddController {
+
+    @FXML
+    private TextField Song;
+
+    @FXML
+    private TextField Artist;
+
+    @FXML
+    private TextField Year;
+
+    @FXML
+    private TextField Album;
+
+}
