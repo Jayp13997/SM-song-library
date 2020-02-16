412cba4459fcebf96eadc400616d9afe19a28e5b
jayp13997 <jayp13997@gmail.com>
2020-02-16 18:17:55 -0500

    added more stuff

--- SM-song-library/src/Graphics/Controller.java
+++ SM-song-library/src/Graphics/Controller.java
@@ -22,5 +22,8 @@ public class Controller {
 
     @FXML
     private Button Delete;
+    
+    @FXML
+    private Label Error;
 
 }
--- SM-song-library/src/Graphics/EditController.java
+++ SM-song-library/src/Graphics/EditController.java
@@ -1,5 +1,21 @@
 package Graphics;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class EditController {
+
+    @FXML
+    private TextField Song;
+
+    @FXML
+    private TextField Artist;
+
+    @FXML
+    private TextField Year;
+
+    @FXML
+    private TextField Album;
 
 }
