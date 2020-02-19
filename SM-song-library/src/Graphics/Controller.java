99db4ed890779130cebf2915bcf0bb6b6ded06d2
jayp13997 <jayp13997@gmail.com>
2020-02-18 21:25:33 -0500

    Try this new UI out

--- SM-song-library/src/Graphics/Controller.java
+++ SM-song-library/src/Graphics/Controller.java
@@ -1,10 +1,11 @@
 package Graphics;
 
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class Controller {
 
@@ -15,15 +16,27 @@ public class Controller {
     private Label details;
 
     @FXML
+    private AnchorPane hide_show;
+
+    @FXML
+    private TextField Song;
+
+    @FXML
+    private TextField Artist;
+
+    @FXML
+    private TextField Year;
+
+    @FXML
+    private TextField Album;
+
+    @FXML
     private Button Add;
 
     @FXML
-    private Button Edit;
+    private Button Delete;
 
     @FXML
-    private Button Delete;
-    
-    @FXML
-    private Label Error;
+    private Button Edit;
 
 }
